759d0463e0e282d0ce68e22c69049543bc23079f
Patrick Aleite <patrick.aleite@gmail.com>
2026-09-26 01:28:39 +0200

    chore: bump layout version for atrium reconstruction

--- src/game/run/generate-run.ts
+++ src/game/run/generate-run.ts
@@ -2,7 +2,7 @@ import { shuffled } from "./seeded-random";
 import type { BookInstance, InitialRun } from "./types";
 
 export const CATALOG_VERSION = 1;
-export const LAYOUT_VERSION = 3;
+export const LAYOUT_VERSION = 4;
 
 export interface FixedBookPlacement {
   bookId: string;
